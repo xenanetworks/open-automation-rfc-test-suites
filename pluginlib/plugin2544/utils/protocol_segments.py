98158750b3b994af2e66e29bc1ca170b1a39ea33
fpfeng <minifen9@gmail.com>
2022-09-08 11:23:32 +0800

    fix: wrong field name

--- pluginlib/plugin2544/utils/protocol_segments.py
+++ pluginlib/plugin2544/utils/protocol_segments.py
@@ -15,10 +15,10 @@ IPV6_ADDRESS_DST = 'Dest IPv6 Addr'
 
 def setup_segment_ethernet(segment: "ProtocolSegment", src_mac: "BinaryString", dst_mac: "BinaryString", arp_mac: Optional["BinaryString"] = None):
     dst_mac = (dst_mac if not arp_mac or arp_mac.is_all_zero else arp_mac)
-    if not dst_mac.is_all_zero and segment[ETHERNET_ADDRESS_SRC].is_all_zero:
-        segment[ETHERNET_ADDRESS_SRC] = dst_mac
-    if not src_mac.is_all_zero and segment[ETHERNET_ADDRESS_DST].is_all_zero:
-        segment[ETHERNET_ADDRESS_DST] = src_mac
+    if not dst_mac.is_all_zero and segment[ETHERNET_ADDRESS_DST].is_all_zero:
+        segment[ETHERNET_ADDRESS_DST] = dst_mac
+    if not src_mac.is_all_zero and segment[ETHERNET_ADDRESS_SRC].is_all_zero:
+        segment[ETHERNET_ADDRESS_SRC] = src_mac
 
 def setup_segment_ipv4(segment: "ProtocolSegment", src_ipv4: "BinaryString", dst_ipv4: "BinaryString"):
     if segment[IPV4_ADDRESS_SRC].is_all_zero:
