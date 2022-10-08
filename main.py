c54a01351b3f0a26f55ba89a7e0a1b56129d29d7
ArtemConstantinov <y_4ox@yahoo.com>
2022-10-08 10:02:10 +0800

    refactor main.py

--- main.py
+++ main.py
@@ -1,58 +1,71 @@
+from __future__ import annotations
 import asyncio
 import json
-import os
-from xoa_core import types, controller
+import pydantic
+from pathlib import Path
+from typing import (
+    Any,
+    cast,
+)
+from xoa_core import (
+    types,
+    controller,
+)
 from loguru import logger
 
 from xoa_converter.entry import converter
 from xoa_converter.types import TestSuiteType
 
-INPUT_DATA_PATH = os.path.abspath("test/2.v2544")
-JSON_PATH = os.path.abspath("test/hello.json")
+
+DEBUG = True
+BASE_PATH = Path.cwd()
+PLUGINS_PATH = BASE_PATH / "pluginlib"
+INPUT_DATA_PATH = BASE_PATH / "test" / "2.v2544"
+JSON_PATH = BASE_PATH / "test" / "hello.json"
+T_SUITE_NAME = "RFC-2544"
 
 
-async def subscribe_executions(c: controller.MainController):
-    async for msg in c.listen_changes(types.PIPE_EXECUTOR):
+async def subscribe(ctrl: controller.MainController, source: str) -> None:
+    async for msg in ctrl.listen_changes(source):
         logger.debug(msg)
 
 
-async def subscribe(id: str, c: controller.MainController):
-    async for msg in c.listen_changes(id):
-        logger.debug(msg)
+async def start_test(ctrl: controller.MainController, config: dict[str, Any], test_suite_name: str) -> None:
+    try:
+        exec_id = ctrl.start_test_suite(test_suite_name, config, debug_connection=DEBUG)
+    except Exception as err:
+        logger.error(err)
+    else:
+        await subscribe(ctrl, exec_id)
 
 
-async def test():
+async def main() -> None:
     new = [
-        # types.Credentials( product=types.EProductType.VALKYRIE, host="87.61.110.114", password="xena"),
+        # types.Credentials( product=types.EProductType.VALKYRIE, host="87.61.110.114", password=cast(pydantic.SecretStr, "xena")),
         types.Credentials(
-            product=types.EProductType.VALKYRIE, host="192.168.1.198", password="xena"
-        ),  # wrong password
+            product=types.EProductType.VALKYRIE, host="192.168.1.198", password=cast(pydantic.SecretStr, "xena")
+        ),
         # types.Credentials(
-        #     product=types.EProductType.VALKYRIE, host="192.168.1.197", password="xena"
-        # ),  # tester is turned off
-        # types.Credentials( product=types.EProductType.VALKYRIE, host="87.61.110.118", password="xena"),
+        #     product=types.EProductType.VALKYRIE, host="192.168.1.197", password=cast(pydantic.SecretStr, "xena")
+        # ),
+        # types.Credentials( product=types.EProductType.VALKYRIE, host="87.61.110.118", password=cast(pydantic.SecretStr, "xena")),
     ]
     c = await controller.MainController()
-    c.register_lib("./pluginlib")
-    for t in new:
-        await c.add_tester(t)
+    c.register_lib(str(PLUGINS_PATH))
+
+    await asyncio.gather(*[c.add_tester(t) for t in new])
+    asyncio.create_task(subscribe(c, types.PIPE_EXECUTOR))
 
     with open(INPUT_DATA_PATH) as f:
         app_data = f.read()
-        info = c.get_test_suite_info("RFC-2544")
-        new_data = converter(TestSuiteType.RFC2544,  app_data, info["schema"])
-
-    asyncio.create_task(subscribe_executions(c))
-    try:
-        id = c.start_test_suite("RFC-2544", json.loads(new_data), debug_connection=True)
-        
-    except Exception as err:
-        logger.debug(err)
-    else:
-        asyncio.create_task(subscribe(id, c))
+        info = c.get_test_suite_info(T_SUITE_NAME)
+        if not info:
+            logger.error("Test suite is not recognised.")
+            return None
+        new_data = converter(TestSuiteType(T_SUITE_NAME), app_data, info["schema"])
+        config = json.loads(new_data)
+        await start_test(c, config, T_SUITE_NAME)
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(test())
-    loop.run_forever()
+    asyncio.run(main())
