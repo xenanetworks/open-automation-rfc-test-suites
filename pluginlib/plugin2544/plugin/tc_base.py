1afc162b98a5e02c81cbd310504ed0b3d324749e
maureencqm <maureencqm@gmail.com>
2023-04-10 14:11:00 +0800

    remove redundant statistic

--- pluginlib/plugin2544/plugin/tc_base.py
+++ pluginlib/plugin2544/plugin/tc_base.py
@@ -102,7 +102,6 @@ class TestCaseProcessor:
             await asyncio.sleep(const.INTERVAL_SEND_STATISTICS)
         await asyncio.sleep(const.DELAY_STATISTICS)
         data = await aggregate_data(self.resources, params, is_final=True)
-        self.xoa_out.send_statistics(data)
         return data
 
     async def _latency(
@@ -140,8 +139,8 @@ class TestCaseProcessor:
         repetition: int,
     ):
         for rate_percent in test_type_conf.rate_sweep_list:
-            self.resources.set_rate_percent(rate_percent)
             await self.add_learning_steps(current_packet_size)
+            self.resources.set_rate_percent(rate_percent)
             await self.start_test(test_type_conf, current_packet_size)
             params = StatisticParams(
                 test_case_type=test_type_conf.test_type,
